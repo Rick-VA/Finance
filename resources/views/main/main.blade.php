a4584a9584a153e2fd3393d6b560089b283c0be7
Student Rick van Assen <rassen0134@student.landstede.nl>
2022-03-01 10:07:11 +0100

    icon logo

--- resources/views/main/main.blade.php
+++ resources/views/main/main.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pecunia - @yield('title')</title>
     <link rel="stylesheet" href="/css/style.css">
+    <link rel="icon" href="images/icon-finance.png">
 </head>
 
 <nav class="mainnav">
