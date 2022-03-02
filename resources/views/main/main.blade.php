7b1b1416e2b6a9f6f5e4dd97a908409022bfb47f
Student Rick van Assen <rassen0134@student.landstede.nl>
2022-03-02 11:24:09 +0100

    title

--- resources/views/main/main.blade.php
+++ resources/views/main/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pecunia - @yield('title')</title>
+    <title>Finance - @yield('title')</title>
     <link rel="stylesheet" href="/css/style.css">
     <link rel="icon" href="images/icon-finance.png">
 </head>
