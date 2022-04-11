3b46398a6ccb0251cf00e5b0fe1fa5fad75ba325
Student Stefan Rietdijk <srietdij0675@student.landstede.nl>
2022-04-11 11:22:37 +0200

    heb een plek voor chartisan

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -39,5 +39,15 @@
             </div>
         </div>
     </div>
-
+    <div class="py-123">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="category-body">
+                    <strong>&nbsp;add a new category</strong>
+                    <input type="submit" value="&nbsp;Delete&nbsp;" class="deletebutton">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
